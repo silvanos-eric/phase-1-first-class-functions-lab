2db7a1dda9b3bf07c388f7bc1b287c1112dfd32c
Silvanos Eric <ericoderito@gmail.com>
2024-07-10 09:25:43 +0300

    feat: add driver selection utilities
    Add utility functions for selecting drivers and fare calculation in `index.js`:
    - Implement `returnFirstTwoDrivers` and `returnLastTwoDrivers` functions.
    - Define `selectingDrivers` array to hold selection functions.
    - Create `createFareMultiplier` to generate fare multiplier functions.
    - Add `fareDoubler` and `fareTripler` using the multiplier function.
    - Implement `selectDifferentDrivers` to allow flexible driver selection.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (scuberDriversArray) {
+  return [scuberDriversArray[0], scuberDriversArray[1]];
+};
+
+const returnLastTwoDrivers = function (scuberDriversArray) {
+  return [
+    scuberDriversArray[scuberDriversArray.length - 2],
+    scuberDriversArray[scuberDriversArray.length - 1],
+  ];
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+  return (fare) => multiplier * fare;
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(scuberDriversArray, cb) {
+  return cb(scuberDriversArray);
+}
